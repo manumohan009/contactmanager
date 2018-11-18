01156cbf471b503c1dede53e6f39a017803ac3f9
Manu Mohan <manumohan.cvav@gmail.com>
2018-11-18 21:02:39 +0530

    Updates

---
+++ precache-manifest.e7e631ff8ac3dfc1e7de3fcd63702ba0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d3fd7d169b2247dc600",
+    "url": "/manumohan009/contactmanager/static/js/runtime~main.0d3fd7d1.js"
+  },
+  {
+    "revision": "06ae7bef7e9934d2f1fd",
+    "url": "/manumohan009/contactmanager/static/js/main.06ae7bef.chunk.js"
+  },
+  {
+    "revision": "b09214b20af42cc8a27b",
+    "url": "/manumohan009/contactmanager/static/js/1.b09214b2.chunk.js"
+  },
+  {
+    "revision": "b09214b20af42cc8a27b",
+    "url": "/manumohan009/contactmanager/static/css/1.e5ec0c70.chunk.css"
+  },
+  {
+    "revision": "bad2022e34d5320b67513101d1c9642c",
+    "url": "/manumohan009/contactmanager/index.html"
+  }
+];
